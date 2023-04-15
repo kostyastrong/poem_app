09906bf077f191bed824769490844e53fe7f4181
v.d.olennikov <v.d.olennikov@tinkoff.ru>
2023-04-15 16:10:18 +0300

    remove theme_data

--- lib/data/theme/theme_data.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-part 'theme_data.freezed.dart';
-
-@freezed
-class MyThemeData with _$MyThemeData {
-  const factory MyThemeData({required Color buttonTextColor}) = _MyThemeData;
-}
